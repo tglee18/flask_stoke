37a6c745cd2395fb6fb553fb6f595f6f1f974656
tglee18 <lx1121459797@163.com>
2019-12-02 15:41:38 +0800

    测试

--- run.py
+++ run.py
@@ -18,6 +18,7 @@ app.register_blueprint(transactionData, url_prefix="/transactionData")  # 交易
 app.register_blueprint(historyFundFlow, url_prefix="/historyFundFlow")  # 历史资金流向
 app.register_blueprint(moneyFlow, url_prefix="/moneyFlow")  # 详细资金流向
 #test
+#sdsad
 
 if __name__ == '__main__':
     app.run()
