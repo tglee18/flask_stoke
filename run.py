145a7e71065a22e1221d13a33866e08f486235ac
wjj2013326984 <2013326984@qq.com>
2019-12-03 19:47:47 +0800

    #run

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-
-
 from config import app
 from views.moneyFlow import moneyFlow
 from views.login import login
@@ -9,6 +7,8 @@ from views.historyTrade import historyTrade
 from views.register import register
 from views.transactionData import transactionData
 from views.historyFundFlow import historyFundFlow
+from views.stockholder import stockholder
+from views.deal import deal
 
 # 将蓝图注册到app
 app.register_blueprint(analysis, url_prefix="/baseinfo")  # 公司基本信息
@@ -19,6 +19,8 @@ app.register_blueprint(register, url_prefix="/register")  # 注册
 app.register_blueprint(transactionData, url_prefix="/transactionData")  # 交易信息
 app.register_blueprint(historyFundFlow, url_prefix="/historyFundFlow")  # 历史资金流向
 app.register_blueprint(moneyFlow, url_prefix="/moneyFlow")  # 详细资金流向
+app.register_blueprint(stockholder, url_prefix="/stockholder")  #内部持股
+app.register_blueprint(deal, url_prefix="/deal")    #买卖表（买一，卖一）
 
 
 if __name__ == '__main__':
