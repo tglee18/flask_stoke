b4f6c5223ee5c580f86d6d771e5f54462b201527
tglee18 <lx1121459797@163.com>
2019-12-13 12:01:58 +0800

    更新了历史文件bug

--- views/transactionData.py
+++ views/transactionData.py
@@ -67,32 +67,32 @@ def get_data2():
     if data_list:
         code = data_list[0].Code
         code = code[2:]
-        while 1:
-            url = "http://quotes.money.163.com/trade/lsjysj_{}.html#01b07".format(code)
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36'
+        # while 1:
+        url = "http://quotes.money.163.com/trade/lsjysj_{}.html#01b07".format(code)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36'
+        }
+        req = requests.get(url, headers).text
+        html = BeautifulSoup(req, "html5lib")
+        info = html.find('div', class_="inner_box").find('table', class_="table_bg001 border_box limit_sale")
+        tr = info.find_all('tr')
+        view = []
+        for i in tr[1:]:
+            td = i.find_all('td')
+            data_item2 = {
+                "date": td[0].get_text(),  # 日期
+                "open": td[1].get_text(),  # 开盘价
+                "high": td[2].get_text(),  # 最高价
+                "low": td[3].get_text(),  # 最低价
+                "close": td[4].get_text(),  # 收盘价
+                "updown": td[5].get_text(),  # 涨跌额
+                "percent": td[6].get_text(),  # 涨跌幅
+                "volume": td[7].get_text(),  # 成交量
+                "turnover": td[8].get_text(),  # 成交金额
+                "amplitude": td[9].get_text(),  # 振幅
+                "turnover_rate": td[10].get_text()  # 换手率
             }
-            req = requests.get(url, headers).text
-            html = BeautifulSoup(req, "html5lib")
-            info = html.find('div', class_="inner_box").find('table', class_="table_bg001 border_box limit_sale")
-            tr = info.find_all('tr')
-            view = []
-            for i in tr[1:]:
-                td = i.find_all('td')
-                data_item2 = {
-                    "date": td[0].get_text(),  # 日期
-                    "open": td[1].get_text(),  # 开盘价
-                    "high": td[2].get_text(),  # 最高价
-                    "low": td[3].get_text(),  # 最低价
-                    "close": td[4].get_text(),  # 收盘价
-                    "updown": td[5].get_text(),  # 涨跌额
-                    "percent": td[6].get_text(),  # 涨跌幅
-                    "volume": td[7].get_text(),  # 成交量
-                    "turnover": td[8].get_text(),  # 成交金额
-                    "amplitude": td[9].get_text(),  # 振幅
-                    "turnover_rate": td[10].get_text()  # 换手率
-                }
-                view.append(data_item2)
+            view.append(data_item2)
         data_pkg = {"status": "success", "datas": view}
     else:
         data_pkg = {"status": "failed"}
