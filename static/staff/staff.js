fec966aafac1c4751ee80b6cd73b38d470b90565
wjj2013326984 <2013326984@qq.com>
2019-12-27 21:58:35 +0800

    股东可以整一手了

--- static/staff/staff.js
+++ static/staff/staff.js
@@ -1,18 +1,40 @@
 $(function() {
-    let info_string = window.localStorage.getItem("staff_info");
+    function getUrlParam(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); //构造一个含有目标参数的正则表达式对象
+        var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+        if (r != null) return decodeURI(r[2]);
+        return null; //返回参数值
+    };
 
-    let staff_info = JSON.parse(info_string);
-    console.log(staff_info);
+    var companyID = getUrlParam('compID');
+    $('#compIndex').attr('href','/comp_index?compID='+companyID);
+    $('#lsjysj').attr('href','/comp_index?compID='+companyID);
+    $('#lszjlx').attr('href','/comp_index?compID='+companyID);
+    $('#zjlx').attr('href','/zijinliuxiang?compID='+companyID);
+    $('#compAnalysis').attr('href','/analysis?compID='+companyID);
+    $('#compStockHolder').attr('href','/staff?compID='+companyID);
+    $('#compInfo').attr('href','/company?compID='+companyID);
+
     let info_arr = [];
-    let trstr="";
-    for (let i in staff_info) {
-        info_arr.push(staff_info[i]);
-    }
-    console.log(info_arr);
-        $.each(info_arr,function(index,element){
-            trstr=`<tr><td>${info_arr[index]['name']}</td><td>${info_arr[index]['position']}</td><td>${info_arr[index]['cgsl']}</td><td>${info_arr[index]['zhbdrq']}</td></tr>`
-            $("#staff_table").append(trstr);
-        })
+    $.ajax({
+        url: "/stockholder/a",
+        type: "GET",
+        data: {
+            text: companyID
+        },
+        dataType: "json",
+        success: function (data) {
+            for (let i in data.datas) {
+                info_arr.push(data.datas[i]);
+            }
+            let trstr="";
+            $.each(info_arr,function(index,element){
+                trstr=`<tr><td>${info_arr[index]['name']}</td><td>${info_arr[index]['position']}</td><td>${info_arr[index]['cgsl']}</td><td>${info_arr[index]['zhbdrq']}</td></tr>`
+                $("#staff_table").append(trstr);
+            })
+        }
+    });
+
 
 
 });
